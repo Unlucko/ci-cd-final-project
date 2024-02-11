4df47f0b172b0f30dff0e4161fd4d097cdabf679
Unlucko <cesarfsegurap.09@gmail.com>
2024-02-11 11:16:46 -0500

    a

--- service/__init__.py
+++ service/__init__.py
@@ -6,8 +6,10 @@ from flask import Flask
 app = Flask(__name__)
 
 # This must be imported after the Flask app is created
-from service import routes               # pylint: disable=wrong-import-position,cyclic-import
-from service.common import log_handlers  # pylint: disable=wrong-import-position
+from service import routes               # pylint: 
+                        #disable=wrong-import-position,cyclic-import
+from service.common import log_handlers  # pylint: 
+                        #disable=wrong-import-position
 
 log_handlers.init_logging(app, "gunicorn.error")
 
--- service/routes.py
+++ service/routes.py
@@ -75,7 +75,7 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND,/
+        return abort(status.HTTP_404_NOT_FOUND,\
         f"Counter {name} does not exist")
 
     counter = COUNTER[name]
@@ -91,7 +91,7 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND,/
+        return abort(status.HTTP_404_NOT_FOUND,\
          f"Counter {name} does not exist")
 
     COUNTER[name] += 1
